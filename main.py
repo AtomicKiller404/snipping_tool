2a46fb08689c4eef2f5d772bf75b3156f5156d3c
Miha Plibersek <miha.plib@gmail.com>
2025-02-18 23:52:46 +0100

    created a visible icon in the system tray

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
+import time
 import keyboard
+from pystray import Icon, MenuItem, Menu
+from PIL import Image, ImageDraw
 from snippingTool import SnippingTool
+import threading
+
+quit_flag = False  # Use a global flag to control the quit state
+
+def on_quit(icon, item):
+    global quit_flag 
+    quit_flag = True
+    icon.stop()
 
 def snippingTool():
     # Create and start the SnippingTool instance
     snipping_tool = SnippingTool()
 
-# Set up the hotkey listener for starting the SnippingTool
-keyboard.add_hotkey("shift+alt+s", snippingTool)
+def start_hotkeys():
+    # Set up the hotkey listener for starting the SnippingTool
+    keyboard.add_hotkey("shift+alt+s", snippingTool)
+    print("Listening for Shift + Alt + S to start snipping...")
 
-print("Listening for Shift + Alt + S to start snipping... Press Shift + Alt + Q to stop.")
+    # Loop to keep the script running and check for quit flag
+    while not quit_flag:
+        time.sleep(0.1)
 
-# Keep the script running in the background
-keyboard.wait("shift+alt+q")  # Wait indefinitely for the hotkeys
+menu = Menu(MenuItem('Quit', on_quit))
+# icon = Icon("cut-scissor-icon.ico", create_image(), menu=menu)
+icon = Icon("SnippingTool", Image.open("cut-scissor-icon.ico"), menu=menu)
+
+# Run the system tray icon in the background
+tray_thread = threading.Thread(target=icon.run)
+tray_thread.daemon = True
+tray_thread.start()
+
+# Start listening for hotkeys in the main thread
+start_hotkeys()
+
+tray_thread.join()
+
+print("Exiting the program...")
